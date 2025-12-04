10948e0630a4764bf4cdad6f7e7abe44811507e9
harshavardhan314 <hharshavardhan974@gmail.com>
2025-12-04 20:01:36 +0530

    Update Appdownload.jsx

--- frontend/src/components/Appdownload/Appdownload.jsx
+++ frontend/src/components/Appdownload/Appdownload.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './appdownload.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./Appdownload.css";
+import { assets } from "../../assets/assets";
 const Appdownload = () => {
   return (
     <div className="appdownload">
-        
-            <p>For better Exprience Download <br></br>Tomato App</p>
-            <div className="app-images">
-                
-                <img src={assets.play_store} alt="" />
-                <img src={assets.app_store} alt="" />
-            </div>
-     
+      <p>
+        For better Exprience Download <br></br>Tomato App
+      </p>
+      <div className="app-images">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appdownload
+export default Appdownload;
