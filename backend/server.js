1e378a6919b1da547e3268579b7e6c41463a8262
Harshavardhan <hharshavardhan974@gmail.com>
2026-05-16 15:46:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"],
   })
 );
 
